0f6f7ef7022f17084af7ef03ad25a5c209b172e0
SteveJeong <jst0930@gmail.com>
2022-06-19 14:44:19 +0300

    button added

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,14 @@
 <script>
 	export let name;
+  let count = 3;
+  const plus = () => {count = count+1}
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <p>Count : {count}</p>
+  <button on:click={plus}>+</button>
 </main>
 
 <style>
